018c76d1b8199a1019a4e7feb31501b0d24e49a9
morpheus17201 <morpheus17201@gmail.com>
2025-02-13 06:43:45 +0530

    Error handling in a1.py

--- a1.py
+++ a1.py
@@ -1,7 +1,8 @@
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
-#   "requests"
+#   "requests",
+#   "fastapi",
 # ]
 # ///
 
@@ -10,13 +11,33 @@ import subprocess
 import requests
 import sys
 
+from fastapi import HTTPException
+
+
+from base_logger import logger
+
 
 async def run_datagen_script(
     user_email=r"23f2005138@ds.study.iitm.ac.in",
     script_url=r"https://raw.githubusercontent.com/sanand0/tools-in-data-science-public/tds-2025-01/project-1/datagen.py",
 ):
     # Step 1: Download the datagen.py script from the URL
-    #script_url = "https://raw.githubusercontent.com/sanand0/tools-in-data-science-public/tds-2025-01/project-1/datagen.py"
+    # script_url = "https://raw.githubusercontent.com/sanand0/tools-in-data-science-public/tds-2025-01/project-1/datagen.py"
+
+    logger.info(f"Parameters received for run_datagen_script:")
+    logger.info(f"{user_email=}")
+    logger.info(f"{script_url}")
+
+    try:
+        response = requests.get(script_url)
+        response.raise_for_status()
+
+    except Exception as e:
+        logger.critical(
+            f"Error occured while fetching the script from the given URL {e}"
+        )
+        raise HTTPException(status_code=400, detail=str(e))
+
     script_content = requests.get(script_url).text
 
     # Step 2: Save the content to a temporary file (datagen.py)
@@ -47,4 +68,7 @@ async def run_datagen_script(
 
 if __name__ == "__main__":
     user_email = "23f2005138@ds.study.iitm.ac.in"  # Replace with actual email
-    run_datagen_script(user_email)
+    script_url = "https://url_with_error.com"
+    import asyncio
+
+    asyncio.run(run_datagen_script(user_email, script_url))
